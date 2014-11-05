3004666c2670a487e0bd508ece8d43e4bb3fec00
adam abid <ab.adnen@gmail.com>
2014-11-05 17:10:08 +0100

    init development

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,85 @@
-module.exports = function(grunt) {
+var G ={
+  host: "http://amail.com.local/",
+  paths: {
+    src: {},
+    dev: {},
+    prod: {
+      img: "assets/dist/images/prod/"
+    }
+  }
+}
 
+module.exports = function(grunt) {
   // Configuration de Grunt
   grunt.initConfig({
+    // BOWER
+    bower: {
+      install: {
+        options: {
+          //targetDir: './components',
+          //layout: 'byType',
+          //install: true,
+          //verbose: false,
+          //cleanTargetDir: false,
+          //cleanBowerDir: false,
+          //bowerOptions: {}
+        }
+      }
+    },
+    // LESS
   	less: {
       dev: {
         options: {
-
+          banner: '/* \n  Generates stylesheet file \n  Date : <%= grunt.template.today("yyyy-mm-dd HH:MM") %> \n*/'
         },
         files: {
-          "assets/dist/stylesheet/style.css": "assets/src/stylesheet/style.less"
+          "assets/dist/stylesheet/dev/style.css": ["assets/src/stylesheet/style.less"]
         }
       },
       prod: {
         options: {
-          paths: ["assets/css"],
+          //paths: ["assets/css"],
+          compress:true,
           cleancss: true,
+          banner: '/* \n  Generates minified stylesheet file \n  Date : <%= grunt.template.today("yyyy-mm-dd HH:MM") %> \n*/',
           modifyVars: {
-            imgPath: '"http://mycdn.com/path/to/images"',
-            bgColor: 'red'
+            imgPath: '"'+G.host+G.paths.prod.img+'"',
+            bgColor: '#f00'
           }
         },
         files: {
-          "assets/dist/stylesheet/style.css": "assets/src/stylesheet/style.less"
+          "assets/dist/stylesheet/prod/style.min.css": ["assets/src/stylesheet/style.less"]
         }
       }
     },
-    cssmin: {
-      combine: {
-        options: {
-          banner: '/* \n  Minified css file \n  Date : <%= grunt.template.today("yyyy-mm-dd HH:MM") %> \n*/'
-        },
-        files: {
-          'assets/dist/stylesheet/output.min.css': ['assets/dist/stylesheet/style.css', 'assets/dist/stylesheet/test.css']
-        }
+    // JS :: minification and compression
+
+    // WATCH
+    watch: {
+      //scripts: {
+        //files: '**/*.js', // tous les fichiers JavaScript de n'importe quel dossier
+        //tasks: ['concat:dist']
+      //},
+      styles: {
+        files: '**/*.less', // tous les fichiers Sass de n'importe quel dossier
+        tasks: ['app-build']
       }
     }
+
+
   })
 
   // Import package
+  grunt.loadNpmTasks('grunt-bower-task')
   grunt.loadNpmTasks('grunt-contrib-less')
   grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 
   // Définition des tâches Grunt
-  grunt.registerTask('app-dev', ['less:dev', 'cssmin:combine'])
+  grunt.registerTask('app-init', ['bower'])
+  grunt.registerTask('app-dev', ['less:dev'])
+  grunt.registerTask('app-prod', ['less:prod'])
+  grunt.registerTask('app-build', ['app-dev', 'app-prod'])
 
 }
 
